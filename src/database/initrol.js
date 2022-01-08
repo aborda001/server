48df41f4cc9db489a0019a4a2cfd4d4a4d6fbafe
Arnaldo Borda <aborda001@gmail.com>
2022-01-08 14:45:51 -0300

    Commit final(?

--- src/database/initrol.js
+++ src/database/initrol.js
@@ -1,25 +1,28 @@
 import { client } from "../database";
 
 export const createRol = async() => {
-    const querySql = `
-    INSERT INTO Roles 
-        (nombre_rol)
-        VALUES
-        ('Administrador');
-    INSERT INTO Roles 
-        (nombre_rol)
-        VALUES
-        ('Maestro');
-    INSERT INTO Roles 
-        (nombre_rol)
-        VALUES
-        ('Estudiante');
-    `;
-    try {
-        const res = await client.query(querySql);
-        console.log("'Roles' is successfully created");
-    } catch (err) {
-        console.log(err.stack);
+    let querySql ="SELECT * FROM Roles;";
+    const allRoles = await client.query(querySql);
+    if (allRoles.rowCount == 0){
+        querySql = `
+        INSERT INTO Roles 
+            (nombre_rol)
+            VALUES
+            ('Administrador');
+        INSERT INTO Roles 
+            (nombre_rol)
+            VALUES
+            ('Maestro');
+        INSERT INTO Roles 
+            (nombre_rol)
+            VALUES
+            ('Estudiante');
+        `;
+        try {
+            const res = await client.query(querySql);
+            console.log("'Roles' is successfully created");
+        } catch (err) {
+            console.log(err.stack);
+        }
     }
-    
 }
