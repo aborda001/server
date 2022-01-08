38d9db55572c757852d53cd6715ae8838fa1dad2
Arnaldo Borda <aborda001@gmail.com>
2022-01-08 08:54:04 -0300

    All controllers for roles

--- src/controllers/roles.controller.js
+++ src/controllers/roles.controller.js
@@ -34,13 +34,71 @@ export const getRoles = async(req, res) => {
 }
 
 export const getRoleById = async(req, res) => {
-    
+    try {
+        const { id } = req.params;
+        const querySql =`SELECT * FROM Roles WHERE id_rol = '${id}';`;
+        const role = await client.query(querySql);
+
+        if ( role.rowCount == 0 ) {
+            return res.status(404).json({
+                message : "Role not found"
+            });
+        }
+
+        res.json(role.rows[0]);
+    } catch (error) {
+        res.status(500).json({
+            "error" : error.detail
+        });
+    }
 }
 
 export const updateRoleById = async(req, res) => {
-    
+    try {
+        const { id } = req.params;
+        const { nombre_rol } = req.body;
+        const querySql =`
+        UPDATE Roles SET
+            nombre_rol = '${nombre_rol}'
+        WHERE id_rol = '${id}';
+        `;
+        const student = await client.query(querySql);
+        
+        if ( student.rowCount == 0 ) {
+            return res.status(404).json({
+                message : "Role not found"
+            });
+        }
+        
+        res.json({
+            message : "Role updated succesfully",
+            data: req.body
+        });
+    } catch (error) {
+        res.status(500).json({
+            "error" : error.file
+        });
+    }
 }
 
 export const deleteRoleById = async(req, res) => {
-    
+    try {
+        const { id } = req.params;
+        const querySql =`DELETE FROM Roles WHERE id_rol = '${id}';`;
+        const role = await client.query(querySql);
+
+        if ( role.rowCount == 0 ) {
+            return res.status(404).json({
+                message : "Role not found"
+            });
+        }
+
+        res.json({
+            message : "Role deleted succesfully"
+        });
+    } catch (error) {
+        res.status(500).json({
+            "error" : error.detail
+        });
+    }
 }
